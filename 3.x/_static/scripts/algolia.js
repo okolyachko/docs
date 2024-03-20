7592559a7fc9e36b28e86cf0aa604c042ea246d0
Olga Kolyachko <olga.kolyachko@gmail.com>
2024-03-20 16:51:38 +0400

    Test versions

---
+++ 3.x/_static/scripts/algolia.js
@@ -0,0 +1,15 @@
+docsearch({
+    inputSelector: '.sidebar-search',
+    typesenseCollectionName: 'alan-3.x', 
+    typesenseServerConfig: { 
+      nodes: [{
+        host: 'bd0q6ipkxs57f1jyp-1.a1.typesense.net', // For Typesense Cloud use xxx.a1.typesense.net
+        port: '443',      // For Typesense Cloud use 443
+        protocol: 'https'   // For Typesense Cloud use https
+      }],
+      apiKey: 'vwvUgPJPXAtgMLZD2KQE58eJFbOTQcs1', // Use API Key with only Search permissions
+    },
+    typesenseSearchParams: { // Optional.
+      per_page: 50
+    },
+  });
